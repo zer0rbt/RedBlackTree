a94cfdefabc4dba4766b4864f18486f5a0e02f94
LisaNota <113865762+LisaNota@users.noreply.github.com>
2023-04-08 09:07:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -155,46 +155,6 @@ struct RBTree {
             fixDeleting(p);
     }
 
-    ////////////////////////////////////
-    // НЕ УКРАЛА А ПОЗАИМСТВОВАЛА
-    // p.s. попробуй потестировать этот код, если не будешь успевать пофиксить повороты
-    void rotateLeft(Node<type>* node) {
-        Node<type>* rightChild = node->right;
-        node->right = rightChild->left;
-        if (rightChild->left != nil) {
-            rightChild->left->parent = node;
-        }
-        rightChild->parent = node->parent;
-        if (node->parent == nil) {
-            root = rightChild;
-        } else if (node == node->parent->left) {
-            node->parent->left = rightChild;
-        } else {
-            node->parent->right = rightChild;
-        }
-        rightChild->left = node;
-        node->parent = rightChild;
-    }
-
-    void rotateRight(Node<type>* node) {
-        Node<type>* leftChild = node->left;
-        node->left = leftChild->right;
-        if (leftChild->right != nil) {
-            leftChild->right->parent = node;
-        }
-        leftChild->parent = node->parent;
-        if (node->parent == nil) {
-            root = leftChild;
-        } else if (node == node->parent->right) {
-            node->parent->right = leftChild;
-        } else {
-            node->parent->left = leftChild;
-        }
-        leftChild->right = node;
-        node->parent = leftChild;
-    }
-    ////////////////////////////////////
-
     void right_rotate(Node<type> *pNode) {
 
         if (pNode->parent != nil) {
