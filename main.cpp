ac2a076cf2ce988f2f86c626ce51f213dfa7df17
Fedor Shuvalov <60184559+zer0rbt@users.noreply.github.com>
2023-04-08 04:38:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,12 +14,29 @@ struct Node {
 
 template<class type>
 struct RBTree {
-    Node<type> *root = nullptr;
+    Node<type> *nil = new Node<type>();
+    Node<type> *root = nil;
+
 
     void push(type data) {
         Node<type> *pNode = new Node<type>();
+        pNode->left = nil;
+        pNode->right = nil;
+        pNode->parent = nil;
         pNode->value = data;
-        if (root != nullptr)
+        if (root != nil)
+            push(root, pNode);
+        else root = pNode;
+    };
+
+    void push(type data, bool color) {
+        Node<type> *pNode = new Node<type>();
+        pNode->left = nil;
+        pNode->right = nil;
+        pNode->parent = nil;
+        pNode->value = data;
+        pNode->color = color;
+        if (root != nil)
             push(root, pNode);
         else root = pNode;
     };
@@ -37,6 +54,9 @@ struct RBTree {
     }
 
     void remove(type key) {
+        nil->parent = nullptr;
+
+
         Node<type> *p = root;
         // находим узел с ключом key
         while (p->value != key) {
@@ -44,88 +64,151 @@ struct RBTree {
                 p = p->right;
             else
                 p = p->left;
+
         }
         // у p нет детей
-        if (p->right == nullptr && p->left == nullptr) {
+        if (p->right == nil && p->left == nil) {
             if (p == root)
-                root = nullptr;
+                root = nil;
             else //ссылку на p у "отца" меняем на nil
-            { if (p->parent->right == p) p->parent->right; else p->parent->left; }
+            { if (p->parent->right == p) p->parent->right = nil; else p->parent->left = nil; }
+            nil->parent = p->parent;
+            fixDeleting(nil);
             return;
         }
 
-        Node<type> y = nullptr;
-        Node<type> q = nullptr;
-        if (p->right == nullptr or p->left == nullptr)
-            p->parent == (p->right == nullptr)?p->left:p->right; //ссылку на у от "отца" меняем на ребенка y
-        else{
-        // два ребенка
-        y = вершина, со следующим значением ключа // у нее нет левого ребенка
-        if y имеет правого ребенка
-        y.right.parent = y.parent
-        if y — корень
-                root = y.right
-        else
-        у родителя ссылку на y меняем на ссылку на первого ребенка y}
-        if y != p
-        p.colour = y.colour
-        p.key = y.key
-        if y.colour == black
-        // при удалении черной вершины могла быть нарушена балансировка
-        fixDeleting(q)
+        Node<type> *y = p;
+        if (p->right == nil or p->left == nil)
+            p->parent = (p->right == nil) ? p->left : p->right; //ссылку на у от "отца" меняем на ребенка y
+        else {
+            // два ребенка
+            while (y->left != nil) y = y->left;
+            if (y->right != nil) //y имеет правого ребенка
+                y->right->parent = y->parent;
+            if (y == root)//y — корень
+                root = y->right;
+            else y->parent = y->right; //у родителя ссылку на y меняем на ссылку на первого ребенка y
+
+        }
+        bool go = (p->color == false);
+        if (y != p) {
+            p->color = y->color;
+            p->value = y->value;
+        }
+        if (go)
+            // при удалении черной вершины могла быть нарушена балансировка
+            fixDeleting(p);
     }
+
 
     void right_rotate(Node<type> *pNode) {
 
-        if (pNode->parent != nullptr) {
-            if (pNode->left != nullptr) {
-                pNode->parent->right = pNode->left;
-                pNode->left->parent = pNode->parent;
-            } else pNode->parent->right = nullptr;
-
-            pNode->left = pNode->parent;
-            pNode->parent = pNode->left->parent;
-            if (pNode->parent == nullptr) {
-                root = pNode;
-            } else {
-                if (pNode->left->parent->right == pNode->left)
-                    pNode->left->parent->right = pNode;
-                else pNode->left->parent->left = pNode;
-            }
-            pNode->left->parent = pNode;
-        }
-    };
-
-    void left_rotate(Node<type> *pNode) {
-
-        if (pNode->parent != nullptr) {
-            if (pNode->right != nullptr) {
+        if (pNode->parent != nil) {
+            if (pNode->right != nil) {
                 pNode->parent->left = pNode->right;
                 pNode->right->parent = pNode->parent;
-            } else pNode->parent->left = nullptr;
+            } else pNode->parent->left= nil;
 
             pNode->right = pNode->parent;
             pNode->parent = pNode->right->parent;
-            if (pNode->parent == nullptr) {
+            if (pNode->parent == nil) {
                 root = pNode;
             } else {
-                if (pNode->right->parent->left == pNode->right)
-                    pNode->right->parent->left = pNode;
-                else pNode->right->parent->right = pNode;
+                if (pNode->right->parent->right == pNode->right)
+                    pNode->right->parent->right = pNode;
+                else pNode->right->parent->left = pNode;
             }
             pNode->right->parent = pNode;
         }
     };
 
+    void left_rotate(Node<type> *pNode) {
+
+        if (pNode->parent != nil) {
+            if (pNode->left != nil) {
+                pNode->parent->right = pNode->left;
+                pNode->left->parent = pNode->parent;
+            } else pNode->parent->right= nil;
+
+            pNode->left = pNode->parent;
+            pNode->parent = pNode->left->parent;
+            if (pNode->parent == nil) {
+                root = pNode;
+            } else {
+                if (pNode->left->parent->left == pNode->left)
+                    pNode->left->parent->left = pNode;
+                else pNode->left->parent->right = pNode;
+            }
+            pNode->left->parent = pNode;
+        }
+    };
+
 
 private:
+    Node<type> *brother(Node<type> *pNode) {
+        return (pNode->parent->left == pNode) ? (pNode->parent->right) : (pNode->parent->left);
+    }
+
+    void fixDeleting(Node<type> *p) {
+        // далее родственные связи относительно p
+        while (p->color == false and p != root) {//p — черный узел и не корень
+            if (p->parent->left == p) { //p — левый ребенок
+
+                if (brother(p)->color == true) {//"брат" красный
+                    brother(p)->color = 0;
+                    p->parent->color = 1;
+                    this->left_rotate(brother(p));
+                }
+                if (brother(p)->left->color == 0 and brother(p)->right->color == 0)//
+                    brother(p)->color = 1;
+                else {
+                    if (brother(p)->right->color == 0) { // случай, рассматриваемый во втором подпункте:
+                        brother(p)->left->color = 0;         // "брат" красный с черными правым ребенком
+                        brother(p)->color = 1;
+                        this->right_rotate((brother(p)->left));
+
+                    }
+                    brother(p)->color = p->parent->color;   // случай, рассматриваемый в последнем подпункте
+                    p->parent->color = 0;
+                    brother(p)->right->color = 0;
+                    this->left_rotate(brother(p));
+                    p = root;
+                }
+            } else { // p — правый ребенок
+                // все случаи аналогичны тому, что рассмотрено выше
+                if (brother(p)->color == true) {
+                    brother(p)->right->color = false;
+                    p->parent->color = true;
+                    this->right_rotate(p->parent);
+                }
+                if (brother(p)->left->color == 0 and brother(p)->left->color == 0)//у "брата" черные дети
+                    brother(p)->color = true;
+                else {
+                    if (brother(p)->left->color == 0) {//левый ребенок "брата" черный
+                        brother(p)->left->color = 0;
+                        brother(p)->color = 1;
+                        this->left_rotate((brother(p)->right));
+                    }
+                    brother(p)->color = p->parent->color;
+                    p->parent->color = 0;
+                    brother(p)->left->color = 0;
+                    this->right_rotate(brother(p));
+                    p = root;
+                }
+            }
+        }
+        p->color = 0;
+        root->color = 0;
+
+    };
+
     void push(Node<type> *pNode, Node<type> *pNewNode) {
         if (pNode->value <= pNewNode->value) {
-            if (pNode->right == nullptr) {
+            if (pNode->right == nil) {
                 pNode->right = pNewNode;
                 pNewNode->parent = pNode;
             } else push(pNode->right, pNewNode);
-        } else if (pNode->left == nullptr) {
+        } else if (pNode->left == nil) {
             pNode->left = pNewNode;
             pNewNode->parent = pNode;
         } else push(pNode->left, pNewNode);
@@ -150,7 +233,8 @@ private:
     void pprint(Node<type> *pNode, int bsc) {
         for (int i = 0; i < bsc; i++)
             cout << " ";
-        cout << pNode->value << endl;
+        if (pNode != nil)
+            cout << pNode->value << endl;
         if (pNode->left) pprint(pNode->left, bsc + 1);
         if (pNode->right) pprint(pNode->right, bsc + 1);
 
@@ -162,15 +246,21 @@ private:
 
 int main() {
     RBTree<int> intBT;
-    vector<int> a = {6, 4, 2, 6, 8, 1, 9, 2, 10, 0};
+    vector<int> a = {6, 3, 8, 2, 5, 7, 79, 8};
 
-    for (int i = 0; i < a.size(); i++)
-        intBT.push(a[i]);
+    intBT.push(a[0]);
+    intBT.push(a[1], 1);
+    intBT.push(a[2], 1);
+    intBT.push(a[3]);
+    intBT.push(a[4]);
+    intBT.push(a[5]);
+    intBT.push(a[6]);
+    intBT.push(a[7], 1);
 
-    intBT.pprint();
-    intBT.right_rotate(intBT.root->right);
-    intBT.left_rotate(intBT.root->left);
-    intBT.pprint();
+
+    //for (int i = 0; i < a.size(); i++){
+    //    intBT.push(a[i]);}
+    intBT.remove(7);
     return 0;
 }
 //
